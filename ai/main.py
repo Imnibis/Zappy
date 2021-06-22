6e55988d9a3149b6784c17a28d1bf8ea67d4282e
MidKnight <romain.bouguet@epitech.eu>
2021-06-22 17:44:28 +0200

    [CODE] adding connection to the server + fixing classes (just adding self. statement in front of vars) + getting initials values from the server

--- ai/main.py
+++ ai/main.py
@@ -6,22 +6,26 @@ import socket
 
 class Map():
     def __init__(self) -> None:
-        msz = [0, 0]                # map size: X Y
+        self.msz = [0, 0]                # map size: X Y
 
 class Player():
     def __init__(self) -> None:
-        pn = 0
-        ppo = [0,  0]               # player position: X Y O
-        plv = 1                     # player level
-        pin = [0, 0, 0, 0, 0, 0, 0] # player inventory
-        tna =  list()               # name of all the teams
+        self.pn = 0
+        self.ppo = [0,  0]               # player position: X Y O
+        self.plv = 1                     # player level
+        self.pin = [0, 0, 0, 0, 0, 0, 0] # player inventory
+        self.tna =  list()               # name of all the teams
 
 class Socket():
     def __init__(self) -> None:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def connect(self, host:str, port:int) -> None:
-        self.sock.connect((host, port))
+        try:
+            self.sock.connect((host, port))
+        except ConnectionRefusedError:
+            stderr.write("ConnectionRefusedError: can't connect to server\n")
+            xit(84)
 
     def send(self, msg:str) -> None:
         self.sock.send(msg.encode())
@@ -31,15 +35,16 @@ class Socket():
         while (len(msg) < 100):
             chunk = self.sock.recv(100 - len(msg))
             if chunk.find(10) != -1:
-                msg += str(chunk)
+                msg += str(chunk, "utf-8")
                 return msg
-        msg += str(chunk)
+        msg += str(chunk, "utf-8")
         return msg
 
 class Client():
     def __init__(self) -> None:
         self.port = None
         self.name = None
+        self.can_connect = None
         self.machine = "localhost"
         self.get_info(len(av))
 
@@ -67,9 +72,38 @@ class Client():
                 stderr.write("Error: Bad argument -> check --help for more info\n")
                 xit(84)
 
+def connection(cli:Client, sock:Socket, map_info:Map) -> None:
+    sock.connect(cli.machine, cli.port)
+    output = sock.receive()
+    if (output == "WELCOME\n"):
+        sock.send(cli.name + '\n')
+        output = sock.receive()
+        splitted = output.split('\n')
+        try:
+            cli.can_connect = int(splitted[0])
+        except ValueError:
+            stderr.write("ValueError: bad value given for CLIENT-NUM\n")
+            xit(84)
+        splitted = splitted[1].split(' ')
+        try:
+            map_info.msz[0] = int(splitted[0])
+            map_info.msz[1] = int(splitted[1])
+        except ValueError:
+            stderr.write("ValueError: bad value given for TEAM-NAME\n")
+            xit(84)
+    elif (output == "ko\n"):
+        stderr.write("ServerError: any welcome messages ... rude\n")
+        sock.sock.close()
+        xit(84)
+
 def main():
-    client = Client()
+    cli = Client()
     sock = Socket()
+    map_info = Map()
+    connection(cli, sock, map_info)
+
+
+
 
 def helper():
     stdout.write("USAGE:\t./zappy_ai -p port -n name -h machine\n"
@@ -83,8 +117,6 @@ def cmd_checkout() -> None:
         xit(84)
     if (av[1] == "--help"):
         helper()
-    for i in av:
-        pass
 
 if __name__ == '__main__':
     try:

97f4a22b9d0b6bc290c1a6fff92d54e46f490d31
Tharegorn <jamie-antoine.ponchon@epitech.eu>
2021-06-22 18:55:44 +0200

    add ia movements

---
+++ server/src/move_ia.c
@@ -0,0 +1,80 @@
+/*
+** EPITECH PROJECT, 2021
+** zapp [WSL: Ubuntu]
+** File description:
+** move_ia
+*/
+
+#include "server.h"
+
+void forward(server_t *s, map_t *m, __attribute__((unused))int re)
+{
+    if (s->players->dir == SOUTH) {
+        if (s->players->y + 1 > m->height - 1)
+            s->players->y = m->height - 1;
+        else 
+            s->players->y += 1;
+    } else if (s->players->dir == NORTH) {
+        if (s->players->y - 1 < 0)
+            s->players->y = 0;
+        else 
+            s->players->y -= 1;
+    }
+    if (s->players->dir == EAST) {
+        if (s->players->x + 1 > m->width - 1)
+            s->players->x = 0;
+        else 
+            s->players->x += 1;
+    } else if (s->players->dir == WEST) {
+        if (s->players->x - 1 < 0)
+            s->players->x = m->width - 1;
+        else 
+            s->players->x -= 1;
+    }
+}
+
+void right(server_t *s, __attribute__((unused))map_t *m, __attribute__((unused)) int re)
+{
+        switch (s->players->dir) {
+        case EAST:
+            s->players->dir = SOUTH;
+            break;
+        case NORTH:
+            s->players->dir = EAST;
+            break;
+        case WEST:
+            s->players->dir = NORTH;
+            break;
+        case SOUTH:
+            s->players->dir = WEST;
+            break;
+        default:
+            break;
+    }
+    dprintf(gui_fd, "ppo %d %d %d %d\n", s->players->pos, s->players->x,
+    s->players->y, s->players->dir);
+    dprintf(s->players->fd, "ok\n");
+}
+
+void left(server_t *s, __attribute__((unused))map_t *m, __attribute__((unused))int re)
+{
+    switch (s->players->dir) {
+        case EAST:
+            s->players->dir = NORTH;
+            break;
+        case NORTH:
+            s->players->dir = WEST;
+            break;
+        case WEST:
+            s->players->dir = SOUTH;
+            break;
+        case SOUTH:
+            s->players->dir = EAST;
+            break;
+        default:
+            break;
+    }
+    dprintf(gui_fd, "ppo %d %d %d %d\n", s->players->pos, s->players->x,
+    s->players->y, s->players->dir);
+    dprintf(s->players->fd, "ok\n");
+}

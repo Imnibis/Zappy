4742afbb4ec7eb74f1b9168fb9e3b1a33c314500
Jamie <jamie-antoine.ponchon@epitech.eu>
2021-06-24 14:57:30 +0200

    modify connect nbr command

--- server/src/request_inventory.c
+++ server/src/request_inventory.c
@@ -9,7 +9,11 @@
 
 void request_inventory(server_t *s, __attribute__((unused))map_t *m, __attribute__((unused))char *elem)
 {
-    dprintf(s->players->fd, "[ food %d, linemate %d, deraumere %d, sibur %d, mendiane %d, phiras %d, thystame %d ]\n", s->players->inventory[0],s->players->inventory[1],s->players->inventory[2],s->players->inventory[3],s->players->inventory[4],s->players->inventory[5],s->players->inventory[6]);
+    dprintf(s->players->fd, "[ food %d, linemate %d, deraumere %d, sibur %d, mendiane %d, phiras %d, thystame %d ]\n",
+    s->players->inventory[0], s->players->inventory[1],
+    s->players->inventory[2], s->players->inventory[3],
+    s->players->inventory[4], s->players->inventory[5],
+    s->players->inventory[6]);
 }
 
 void broadcast(__attribute__((unused))server_t *s, __attribute__((unused)) map_t *m, __attribute__((unused))char *elem)
@@ -19,5 +23,5 @@ void broadcast(__attribute__((unused))server_t *s, __attribute__((unused)) map_t
 
 void connect_nbr(__attribute__((unused))server_t *s, __attribute__((unused)) map_t *m, __attribute__((unused))char *elem)
 {
-    dprintf(s->players->fd, "0\n");
+    dprintf(s->players->fd, "%d\n", (s->cli_max - s->nb_cli) + 1);
 }

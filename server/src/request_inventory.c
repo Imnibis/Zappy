eab49974983a371b2cc72ef7ab2647d7e104df97
Jamie <jamie-antoine.ponchon@epitech.eu>
2021-06-24 14:31:56 +0200

    change invenroty command

--- server/src/request_inventory.c
+++ server/src/request_inventory.c
@@ -9,9 +9,7 @@
 
 void request_inventory(server_t *s, __attribute__((unused))map_t *m, __attribute__((unused))char *elem)
 {
-    printf("inventory passed\n");
     dprintf(s->players->fd, "[ food %d, linemate %d, deraumere %d, sibur %d, mendiane %d, phiras %d, thystame %d ]\n", s->players->inventory[0],s->players->inventory[1],s->players->inventory[2],s->players->inventory[3],s->players->inventory[4],s->players->inventory[5],s->players->inventory[6]);
-    printf("[food %d, linemate %d, deraumere %d, sibur %d, mendiane %d, phiras %d, thystame %d]\n", s->players->inventory[0],s->players->inventory[1],s->players->inventory[2],s->players->inventory[3],s->players->inventory[4],s->players->inventory[5],s->players->inventory[6]);
 }
 
 void broadcast(__attribute__((unused))server_t *s, __attribute__((unused)) map_t *m, __attribute__((unused))char *elem)

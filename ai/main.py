67755006e7cdd6aa818920d54e4a73484d1818d5
MidKnight <romain.bouguet@epitech.eu>
2021-06-24 19:18:43 +0200

    deleting an unused var

--- ai/main.py
+++ ai/main.py
@@ -17,7 +17,6 @@ class Map():
 
 class Player():
     def __init__(self) -> None:
-        self.pn = 0
         self.ppo = [0,  0]               # player position: X Y O
         self.plv = 1                     # player level
         self.pin = [0, 0, 0, 0, 0, 0, 0] # player inventory

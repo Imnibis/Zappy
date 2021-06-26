ab89f97d65f2a2998e5bbe44144fa38e814c96e9
MidKnight <romain.bouguet@epitech.eu>
2021-06-26 17:59:18 +0200

    removing unused libs

--- ai/AI.py
+++ ai/AI.py
@@ -1,9 +1,7 @@
-from typing import List
 from Client import Client
 from Map import Map
 from Player import Player
 from Socket import Socket
-from sys import stderr
 
 def get_tiles_content(sock:Socket) -> list:
     sock.send("Look\n")
